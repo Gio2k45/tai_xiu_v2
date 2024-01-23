13bf2a1fdfe5320902a06b39e90943a7eae4cb40
Wind + <clonefb2k41@gmail.com>
2024-01-23 11:41:14 +0700

    v2

---
+++ src/settings.22f2f.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/Loading.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"69b9a",main:"47b94"}};
